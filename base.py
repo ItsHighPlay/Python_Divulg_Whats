a7911f126793c3b7f231d9429feabb7352e11be2
ItsHighPlay <itshighplay.inc@gmail.com>
2026-02-09 09:27:14 -0300

    Create base.py
    base of algorith to increscse with another code

---
+++ base.py
@@ -0,0 +1,17 @@
+import pywhatkit as kit
+data_hour = 12h00m01s
+atual_messg = 0
+limit_messg = 100
+
+kit.sendwhatmsg("+WhatsApp number/grupo
+                  -Mesage to invite shop and visit-")
+
+kit.sendwhatmsg(+WhatsApp number/grupo
+                  -Mesage to invite shop and visit-",
+                   data_hour.momento
+                   data_hour.minute
+                   data_hour.second)
+                    
+if sended message send in data_hour.minute + 2
+  while atual+1 else atual_messg = limit_messg
+      stop
